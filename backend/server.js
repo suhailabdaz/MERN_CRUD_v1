71a08519089ccba90d1b9e45233b11ae18798dfe
suhailabdaz <suhail.abdaz@gmail.com>
2024-04-02 10:38:37 +0530

    ui/server intsct-v1-Dx-2

--- backend/server.js
+++ backend/server.js
@@ -3,12 +3,14 @@ import dotenv from 'dotenv'
 import connectDB from './config/mongodb.js'
 import adminRouter from './routes/adminRoutes.js'
 import userRouter from './routes/userRoutes.js'
+import cors from 'cors'
 
 dotenv.config()
 
 
 const PORT = process.env.PORT || 5000
 const app = express();
+app.use(cors())
 connectDB()
 
 app.use('/',userRouter)
